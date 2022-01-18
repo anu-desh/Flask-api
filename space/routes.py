3b6aea3382d8820a9b17e0810b4c61a45687f665
Licious <licious@admins-MacBook-Pro.local>
2022-01-18 12:33:36 +0530

    added few more methods

--- space/routes.py
+++ space/routes.py
@@ -1,4 +1,3 @@
-import imp
 from space import app
 from flask import render_template,request,redirect,flash,url_for,jsonify
 from space.models import User
@@ -18,10 +17,34 @@ def login():
         return redirect(url_for('users'))
     return render_template('login.html')
 
-@app.route('/users')
+@app.route('/users',methods = ['GET'])
 def users():
     users = User.query.all()
     return render_template('users.html', users = users)
+
+@app.route('/delete/<id>',methods = ['DELETE'])
+def delete(id):
+        user = User.query.get(id)
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            flash("Deleted successfully!")
+        else:
+            flash("User not found")
+        return redirect(url_for('users'))
+
+@app.route('/put/<id>',methods = ['PUT'])
+def put(id):
+    user = User.query.get(id)
+    name = request.args.get("name")
+    email = request.args.get("email")
+    if name:
+        user.name = name
+    if email:
+        user.email = email
+    db.session.commit()
+    flash("User updated!")
+    return redirect(url_for('users'))
 
 @app.route('/json')
 def json():
